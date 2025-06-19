6ab300079e993bfc49100d1330deafd8b1c85fd3
iwdkmp <iwdkmp@gmail.com>
2025-06-19 13:50:36 +0300

    create docerfile and add exeption into async def get_hh_vacancies()

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import asyncio
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
+TIMEOUT = httpx.Timeout(15.0)
+
 app = FastAPI()
 
 
@@ -14,7 +16,7 @@ async def hello(name: str):
 
 
 @app.get("/hello/{name}")
-async def hello(name: str):
+async def hello_with_name(name: str):
     return {"message": f"Hi, {name}!"}
 
 
@@ -38,7 +40,7 @@ async def slow_endpoint(count: int = 500000000):
 
 
 async def fetch_vacancies(url, headers):
-    async with httpx.AsyncClient(timeout=15) as client:
+    async with httpx.AsyncClient(timeout=TIMEOUT) as client:
         response = await client.get(url, headers=headers)
         return response.json()
 
@@ -51,27 +53,32 @@ async def extract_vacancies_links(data):
     return name_and_link
 
 
-@app.get("/hh_vacancies/{job_name}/")
+@app.get("/hh_vacancies/{job_name}")
 async def get_hh_vacancies(job_name, pages: int = 20):
     headers = {"User-Agent": "Mozilla/5.0"}
     url = f"https://api.hh.ru/vacancies" \
           f"?text={job_name}" \
           f"&search_field=name" \
           f"&per_page={pages}"
-    vacancies_data = await fetch_vacancies(url=url, headers=headers)
-    links = await extract_vacancies_links(vacancies_data)
-    return {"vacancies_title_and_link": links}
+    try:
+        vacancies_data = await fetch_vacancies(url=url, headers=headers)
+        links = await extract_vacancies_links(vacancies_data)
+        return {"vacancies_title_and_link": links}
+    except httpx.RequestError as e:
+        return {"error": f"Request failed: {str(e)}"}
+    except Exception as e:
+        return {"error": f"Unexpected error: {str(e)}"}
 
 
 class User(BaseModel):
-    name: str = "Biba"
-    age: int = 21
-    email: EmailStr = "fightfucker@gmail.com"
+    name: str
+    age: int
+    email: EmailStr
 
 
-@app.post("/crate_user")
+@app.post("/create_user")
 async def create_user(user: User):
-    return {"Message": f"{user.name} | {user.age} y.o. | {user.email}, "}
+    return {"message": f"{user.name} | {user.age} y.o. | {user.email}, "}
 
 
 def loop_sleep(count):
