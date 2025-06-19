ba5114dec38000ab757843f432d71e1de2127562
iwdkmp <iwdkmp@gmail.com>
2025-06-19 14:18:40 +0300

    Cleared main of garbage

--- src/main.py
+++ src/main.py
@@ -1,56 +1,10 @@
-import time
-
 import httpx
 import asyncio
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
 
 TIMEOUT = httpx.Timeout(15.0)
 
 app = FastAPI()
-
-
-@app.get("/hello")
-async def hello(name: str):
-    return {"message": f"Hello, {name}!"}
-
-
-@app.get("/hello/{name}")
-async def hello_with_name(name: str):
-    return {"message": f"Hi, {name}!"}
-
-
-@app.get("/slow")
-async def slow_endpoint(seconds: int = 10):
-    start = time.time()
-    await asyncio.sleep(seconds)
-    end = time.time()
-    return {
-        "message": f"Проспал {seconds} секунд",
-        "duration": round(end - start, 2)
-    }
-
-
-@app.get("/slow_loop")
-async def slow_endpoint(count: int = 500000000):
-    await asyncio.get_running_loop().run_in_executor(None, loop_sleep, count)
-    return {
-        "message": f"прошел {count} "
-    }
-
-
-async def fetch_vacancies(url, headers):
-    async with httpx.AsyncClient(timeout=TIMEOUT) as client:
-        response = await client.get(url, headers=headers)
-        return response.json()
-
-
-async def extract_vacancies_links(data):
-    name_and_link = [
-        [item.get("name", ""), item.get("alternate_url", "")]
-        for item in data.get("items", [])
-    ]
-    return name_and_link
 
 
 @app.get("/hh_vacancies/{job_name}")
@@ -70,17 +24,15 @@ async def get_hh_vacancies(job_name, pages: int = 20):
         return {"error": f"Unexpected error: {str(e)}"}
 
 
-class User(BaseModel):
-    name: str
-    age: int
-    email: EmailStr
+async def fetch_vacancies(url, headers):
+    async with httpx.AsyncClient(timeout=TIMEOUT) as client:
+        response = await client.get(url, headers=headers)
+        return response.json()
 
 
-@app.post("/create_user")
-async def create_user(user: User):
-    return {"message": f"{user.name} | {user.age} y.o. | {user.email}, "}
-
-
-def loop_sleep(count):
-    for i in range(count):
-        pass
+async def extract_vacancies_links(data):
+    name_and_link = [
+        [item.get("name", ""), item.get("alternate_url", "")]
+        for item in data.get("items", [])
+    ]
+    return name_and_link
